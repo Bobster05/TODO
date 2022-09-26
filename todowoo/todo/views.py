11d73267eb4521ae809cab7bdd07dcfb570f10f2
Bobster05 <100536341+Bobster05@users.noreply.github.com>
2022-09-26 22:05:08 +0300

    complete and delete todos

--- todowoo/todo/views.py
+++ todowoo/todo/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
 from .forms import TodoForm
 from .models import Todo
+from django.utils import timezone
 
 
 def home(request):
@@ -30,7 +31,7 @@ def signupuser(request):
 
 
 def currenttodos(request):
-    todos = Todo.objects.filter(user=request.user)
+    todos = Todo.objects.filter(user=request.user, datecomleted__isnull=True)
     return render(request, 'todo/currenttodos.html', {'todos': todos})
 
 
@@ -61,7 +62,37 @@ def createtodo(request):
             return render(request, 'todo/createtodo.html', {'form': TodoForm(),
                                                             'error': 'Bad data passed in. Try again.'})
 
+
 def logoutuser(request):
     if request.method == 'POST':
         logout(request)
         return redirect('home')
+
+
+def viewtodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == "GET":
+        form = TodoForm(instance=todo)
+        return render(request, 'todo/viewtodo.html', {'todo': todo, 'form':form})
+    else:
+        try:
+            form = TodoForm(request.POST, instance=todo)
+            form.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request, 'todo/viewtodo.html', {'todo': todo, 'form': form, 'error': 'Bad info'})
+
+
+def completetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == "POST":
+        todo.datecomleted = timezone.now()
+        todo.save()
+        return redirect('currenttodos')
+
+
+def deletetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == "POST":
+        todo.delete()
+        return redirect('currenttodos')
--- todowoo/todowoo/urls.py
+++ todowoo/todowoo/urls.py
@@ -25,7 +25,10 @@ urlpatterns = [
     path('login/', views.loginuser, name='loginuser'),
 
     #   todos
+    path('', views.home, name='home'),
     path('create/', views.createtodo, name='createtodo'),
     path('current/', views.currenttodos, name='currenttodos'),
-    path('', views.home, name='home'),
+    path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
+    path('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'),
+    path('todo/<int:todo_pk>/delete', views.deletetodo, name='deletetodo'),
 ]
